b7ea9cdc2971ca0d85a14228e0d9ab650084c40e
divyanshusgit <146812228+divyanshusgit@users.noreply.github.com>
2023-12-06 14:41:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+var aTags = document.querySelectorAll('span[data-href]');
+for(var i = 0; i < aTags.length; i++){
+    var aTag = aTags[i];
+    aTag.addEventListener('click', function(e){
+        var ele = e.target;
+        window.location.replace(ele.getAttribute('data-href'));
+    });    
+}
